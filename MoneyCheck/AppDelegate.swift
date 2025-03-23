5a3f9a6e9b2c4e76745da5b7406e347c4f974e9d
kabulov <nkabulov@fortebank.com>
2025-03-23 16:34:57 +0500

    project was created

---
+++ MoneyCheck/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MoneyCheck
+//
+//  Created by Нурсултан Кабулов on 23.03.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ MoneyCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyCheck
+//
+//  Created by Нурсултан Кабулов on 23.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
